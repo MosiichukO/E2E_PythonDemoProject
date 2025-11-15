1f74547e6ea845020cbfc2a6f7316201179cd47a
Oleksandr Mosiichuk <mosiychuk1998a@gmail.com>
2025-11-16 00:09:30 +0200

    add basePage

---
+++ pages/BasePage.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import Page
+
+
+class BasePage:
+    def __init__(self, page: Page):
+        self.page = page
+
+    def navigate(self, url: str):
+        """Навігація на URL"""
+        self.page.goto(url, wait_until="domcontentloaded")
+
+    def click (self, locator: str):
+        self.page.wait_for_selector(locator, state="visible")
+        self.page.click(locator)
+
+
